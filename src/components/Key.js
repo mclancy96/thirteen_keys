f57aef77a7d9cd1f282f6a2450dad08cc2feb729
Mike <mclancy96@gmail.com>
2021-08-16 21:13:51 -0400

    Add background

--- src/components/Key.js
+++ src/components/Key.js
@@ -4,9 +4,24 @@ class Key extends Component {
     render(){
         const {keyData} = this.props;
         return(
-            <div className="container">
-                <div className="text-start">
-                    <h5>{keyData.title}</h5>
+            <div className="container text-start my-2">
+                <div className="row my-0">
+                    <div className="col-1">
+                        <div className="form-check form-switch">
+                           <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault" defaultChecked={keyData.status}></input>
+                        </div>
+                    </div>
+                    <div className="col-11">
+                        <h5>{keyData.title}</h5>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-1">
+
+                    </div>
+                    <div className="col-11 pl-3">
+                        <span>{keyData.description}</span>
+                    </div>
                 </div>
             </div>
         )
